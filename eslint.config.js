33a8f872e1d104d1376cf39862cccb61c5c42a21
RaphaelGuerra <raphaelguerra88@gmail.com>
2025-11-29 13:36:28 -0500

    chore: add flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactHooks from "eslint-plugin-react-hooks";
+
+const tsRecommended = tsPlugin.configs.recommended;
+const hooksRecommended = reactHooks.configs.recommended;
+
+export default [
+  {
+    ignores: ["dist", "node_modules", "public/*.png"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        fetch: "readonly",
+        localStorage: "readonly",
+        clearTimeout: "readonly",
+        HTMLInputElement: "readonly",
+        Blob: "readonly",
+        URL: "readonly",
+        File: "readonly",
+        HTMLCanvasElement: "readonly",
+        CanvasRenderingContext2D: "readonly",
+        StorageEvent: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...(tsRecommended?.rules ?? {}),
+      ...(hooksRecommended?.rules ?? {}),
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
